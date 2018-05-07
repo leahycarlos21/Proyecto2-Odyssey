8f2400f4acd49b4179f3bbf74224200abdc5a4f8
leahycarlos21 <leahyvasquez@gmail.com>
2018-05-06 23:36:22 -0600

    Reacomodo de clases
    Reacomodo de la clase AddDatoMensaje y cambio de nombre.

--- src/com/tec/datos1/FuncionesServer/CancionAlmacenaje.java
+++ src/com/tec/datos1/FuncionesServer/CancionAlmacenaje.java
@@ -8,7 +8,9 @@ public class CancionAlmacenaje {
 
     public boolean almacenarCacion(){
         boolean almacenadoExito=true;
-
+/**
+ * Que al entrar lo convierta en Json y agregar una lista
+ */
 
 
         return almacenadoExito;
