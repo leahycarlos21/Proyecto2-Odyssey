5a37ba17e9b5a0c6ad7bd5d7330c8ec1e253ddea
leahycarlos21 <leahyvasquez@gmail.com>
2018-05-06 22:04:16 -0600

    Socket server con cliente
    Recibe el xml y los transforma a objeto.

---
+++ src/com/tec/datos1/Mensajes.java
@@ -0,0 +1,17 @@
+package com.tec.datos1;
+
+public class Mensajes {
+    public String OpCod;
+
+    public Mensajes(){
+
+    }
+
+    public String getOpCod() {
+        return OpCod;
+    }
+
+    public void setOpCod(String opCod) {
+        OpCod = opCod;
+    }
+}
