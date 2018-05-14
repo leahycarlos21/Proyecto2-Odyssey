1770f6998df7d79711c324a1f23683d5385f6e9c
leahycarlos21 <leahyvasquez@gmail.com>
2018-05-13 18:36:50 -0600

    Implementación Lista Enlazada, config. Kevin
    Se creó una lista para mantener en memoria las canciones y se sincronizan con un archivo json, al inicial y cerrar la aplicación.

--- src/com/tec/datos1/XMLCancion/Canciones.java
+++ src/com/tec/datos1/XMLCancion/Canciones.java
@@ -2,20 +2,20 @@ package com.tec.datos1.XMLCancion;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-@JsonIgnoreProperties(value={ "PRUEBAA" }, allowGetters=true)
+//@JsonIgnoreProperties(value={ "nil" }, allowGetters=true)
 public class Canciones {
     public String artista;
     public String album;
     public String nombreCancion;
     public String genero;
     public String letra;
-    public byte bytesSong;
+    public byte[] bytesSong;
 
     public Canciones(){
 
     }
 
-    public Canciones(String artista, String album, String nombreCancion, String genero, String letra, byte byteSong) {
+    public Canciones(String artista, String album, String nombreCancion, String genero, String letra, byte[] byteSong) {
         this.artista = artista;
         this.album = album;
         this.nombreCancion = nombreCancion;
